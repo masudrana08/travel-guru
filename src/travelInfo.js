7019ab678951de7618facc4390921ab86b2c929c
masudrana08 <rana028511@gmail.com>
2020-09-16 09:40:04 +0600

    setup done

--- src/travelInfo.js
+++ src/travelInfo.js
@@ -1,31 +1,31 @@
-// https://i.ibb.co/p1Fm5yD/coxsbazar.png
-// https://i.ibb.co/pn1Pqr2/hotel2.png
-// https://i.ibb.co/Ttq3ZCq/hotel3.png
-// https://i.ibb.co/QvD0nVJ/hotel1.png
-// https://i.ibb.co/P9sLmbR/sajek.jpg
-// https://i.ibb.co/ZSDZQPB/sreemongol.jpg
-// https://i.ibb.co/mSBp1M0/sundarban.jpg
+https://i.ibb.co/p1Fm5yD/coxsbazar.png
+https://i.ibb.co/pn1Pqr2/hotel2.png
+https://i.ibb.co/Ttq3ZCq/hotel3.png
+https://i.ibb.co/QvD0nVJ/hotel1.png
+https://i.ibb.co/P9sLmbR/sajek.jpg
+https://i.ibb.co/ZSDZQPB/sreemongol.jpg
+https://i.ibb.co/mSBp1M0/sundarban.jpg
 
 const touristPlace=[
     {
         id:1,
         title:"Cox's Bazar",
         description:"Why Cox's Bazar is a Great Tourist Attraction Cox's Bazar Review. Cox's Bazar is famous for its long natural sandy sea beach. ... Cox's Bazar has the world's largest unbroken sea beach which stretches more than 120 km. The entire beach is a stretch of golden sandy sea beach which is reachable by motorbike.",
-        img:""
+        img:"https://i.ibb.co/p1Fm5yD/coxsbazar.png"
 
     },
     {
         id:2,
         title:"Sreemongol",
         description:"It is famous for one of the largest tea gardens of the world which covers with lush green carpet. Most of the tea estates of Bangladesh are in Sreemangal. That's why this town is called the land of two leaves and a bud. It is also familiar as the tea capital of Bangladesh because of many tea estates.",
-        img:""
+        img:"https://i.ibb.co/ZSDZQPB/sreemongol.jpg"
 
     },
     {
         id:3,
         title:"Sundarban",
         description:"Sundarbans is the only place in the world where the Ganges river dolphins and Irrawaddy dolphins are found. These dolphins are among the world's most endangered mammals. ... While visiting the mangrove forest, stop by on one of these places and watch the dolphins jumping from the water. It will be a great experience.",
-        img:""
+        img:"https://i.ibb.co/mSBp1M0/sundarban.jpg"
 
     }
 
@@ -40,7 +40,8 @@ const hotelInfo=[
         guest:4,
         bedroom:2,
         bed:2,
-        bath:2
+        bath:2,
+        img:"https://i.ibb.co/QvD0nVJ/hotel1.png"
     },
     {
         id:2,
@@ -50,7 +51,8 @@ const hotelInfo=[
         guest:4,
         bedroom:2,
         bed:2,
-        bath:2
+        bath:2,
+        img:"https://i.ibb.co/pn1Pqr2/hotel2.png"
     },
     {
         id:3,
@@ -60,7 +62,8 @@ const hotelInfo=[
         guest:4,
         bedroom:2,
         bed:2,
-        bath:2
+        bath:2,
+        img:"https://i.ibb.co/Ttq3ZCq/hotel3.png"
     }
     
 ]
